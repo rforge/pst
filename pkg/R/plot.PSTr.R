40deaeb2edd1b534a0f7198bb710cbb290f4dd51
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-27 16:47:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.PSTr.R
+++ pkg/R/plot.PSTr.R
@@ -105,33 +105,6 @@ setMethod("plot", "PSTr", function (x, y=missing, max.level=NULL,
 }
 
 
-##
-plotNodeLimit <- function (x1, x2, subtree, max.level) {
-	inner <- !all(subtree@leaf) && x1 != x2 && !subtree@order==max.level
-
-	if (inner) {
-	        ## K <- length(subtree)
-		K <- which.child(subtree)
-        	mTop <- summary(subtree, max.level=max.level)@leaves
-        	limit <- integer(length(K))
-		names(limit) <- K
-        	xx1 <- x1
-
-        	for (k in K) {
-            		m <- summary(subtree[[k]], max.level=max.level)@leaves
-            		xx1 <- xx1 + ((x2 - x1) * m/mTop)
-			limit[k] <- xx1
-		}
-		limit <- c(x1, limit)
-	} else {
-		limit <- c(x1, x2)
-	}
-	## mid <- attr(subtree, "midpoint")
-	mid <- length(subtree)/2
-
-    x <- mean(c(x1, x2))
-    list(x = x, limit = limit)
-}
 
 
 ## 
