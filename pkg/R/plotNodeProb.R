c70a613dc7a6b6f828f4b90c3f0ff3808d03c1d7
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-27 10:35:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plotNodeProb.R
+++ pkg/R/plotNodeProb.R
@@ -1,18 +1,72 @@
 ## Plotting probability distribution as a stacked bar
 
-plotProb <- function(x0, y0, x1, y1, prob, state, cpal) {
-	
-	ptmp <- y0
-	
-	if (!missing(state)) {
-		rect(x0, ptmp, x1, ptmp+prob[state], col=cpal[state])
-	} else {
-		rect(x0, y0, x1, y1)
+plotNodeProb <- function(x0, y0, x1, y1, prob, state, cpal, pruned, horiz=TRUE, axes=FALSE) {
+	## message(" x0=", x0, ",y0=", y0, ",x1=", x1, ",y1=", y1)
 
-		for (s in 1:length(prob)) {
-			rect(x0, ptmp, x1, ptmp+prob[s], col=cpal[s])
-			ptmp <- ptmp+prob[s]
+	A <- colnames(prob)
+	xsize <- x1-x0
+	ysize <- y1-y0
+
+	rect(x0, y0, x1, y1, col="lightgray", border=NA)
+
+	if (!horiz) {
+		pdim <- ysize/nrow(prob)
+		ybot <- y0
+
+		for (pos in 1:nrow(prob)) {
+			ytop <- ybot+pdim
+
+			xtmp <- x0
+			if (!any(is.na(prob[pos,]))) {
+				for (s in 1:length(A)) {
+					xright <- xtmp+(prob[pos, s]*xsize)
+
+					rect(xtmp, ybot, xright, ytop, col=cpal[s], border=NA)
+					xtmp <- xright
+				}
+			
+				if (pruned[pos]) {
+					segments(x0, y0, x1, y1,
+						col = "red", ## lty = lty, lwd = lwd
+					)
+				}
+			}
+			ybot <- ytop
+		}
+	} else {
+		pdim <- xsize/nrow(prob)
+		xleft <- x0
+
+		for (pos in 1:nrow(prob)) {
+			xright <- xleft+pdim
+
+			ytmp <- y0
+			if (!any(is.na(prob[pos,]))) {
+				for (s in 1:length(A)) {
+					ybot <- ytmp+(prob[pos, s]*ysize)
+
+					rect(xleft, ytmp, xright, ybot, col=cpal[s], border=NA)
+					ytmp <- ybot
+				}
+			
+				if (pruned[pos]) {
+					segments(x0, y0, x1, y1,
+						col = "red", ## lty = lty, lwd = lwd
+					)
+				}
+			}
+			xleft <- xright
+		}
+		if (axes) {
+			segments(x0, y0-0.1, x1, y0-0.1)
+			segments(x0, y0-0.1, x0, y0-0.125)
+			segments(x1, y0-0.1, x1, y0-0.125)
 		}
 	}
+
+	rect(x0, y0, x1, y1)
+
 }
+
+
  
