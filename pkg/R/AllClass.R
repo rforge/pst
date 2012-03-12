e532179a17b3eee85d8bc7a3501ca2ed71809415
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-12 14:40:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AllClass.R
+++ pkg/R/AllClass.R
@@ -24,22 +24,6 @@ setClass("PSTr",
 	contains="list"
 )
 
-setClass("PSTr.tv",
-	representation(
-		alphabet="character",
-		labels="character",
-		cpal="character",
-		counts="matrix",
-    		n="numeric",
-		prob="matrix",
-		path="character",
-		order="integer",
-		leaf="logical",
-		pruned="logical"),
-	contains="list"
-)
-
-
 setClass("PSTf",
 	representation(
 		data="stslist",
@@ -50,46 +34,6 @@ setClass("PSTf",
 		group="factor"),
 	contains="list"
 )
-
-
-setClass("PSTf.tv",
-	representation(
-		data="stslist",
-		alphabet="character",
-		labels="character",
-		cpal="character"),
-	contains="list"
-)
-
-setClass("PSTf.gr",
-	representation(
-		data="stslist",
-		alphabet="character",
-		labels="character",
-		cpal="character"),
-	contains="list"
-)
-
-
-setClass("PSTf.mc",
-	representation(
-		context.data="stslist",
-		context.alphabet="character",
-		context.labels="character",
-		context.cpal="character"),
-	contains="PSTf"
-)
-
-
-
-setClass("PSTr.root",
-	representation(
-		alphabet="character",
-		labels="character",
-		cpal="character"),
-	contains="PSTr"
-)
-
 
 setClass("PST.summary",
 	representation(
--- pkg/R/AllGeneric.R
+++ pkg/R/AllGeneric.R
@@ -25,79 +25,26 @@ setGeneric(
 )
 
 setGeneric(
-      name="subtree",
-	def=function(object, group)
-	standardGeneric("subtree")
+      name="pstree",
+	def=function(x, group, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, ...)
+	standardGeneric("pstree")
 )
-
-
-setGeneric(
-      name="pstpos",
-	def=function(object, pos)
-	standardGeneric("pstpos")
-)
-
-
-setGeneric(
-      name="pstree.rec",
-	def=function(object, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, method="A", verbose=FALSE)
-	standardGeneric("pstree.rec")
-)
-
-setGeneric(
-      name="pstree.v2",
-	def=function(object, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, method="A", verbose=FALSE)
-	standardGeneric("pstree.v2")
-)
-
 
 setGeneric("query", 
 	def=function(object, string, state, output="prob", exact=FALSE, ...)
 	standardGeneric("query")
 )
 
-
-setGeneric("sim", 
-	def=function(object, seqdata, L, ...) 
-	standardGeneric("sim")
+setGeneric(
+      name="subtree",
+	def=function(object, group)
+	standardGeneric("subtree")
 )
-
-
-setGeneric("typical", 
-	def=function(object, seqdata, l, n, s1, p1, method, L, ...) 
-	standardGeneric("typical")
-)
-
 
 setGeneric(name="tune",
 	def=function(object, K, criterion="AIC", output="PST")
 	standardGeneric("tune")
 )
 
-## Multichannel
-setGeneric("predict.mc", 
-	def=function(object, x, y, L=NULL, p1=NULL, decomp=FALSE, log=FALSE, base=2, norm=FALSE) 
-	standardGeneric("predict.mc")
-)
-
-setGeneric(
-      name="pstree.mc",
-	def=function(x, y, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, ...)
-	standardGeneric("pstree.mc")
-)
-
-## Time varying
-setGeneric(
-      name="pstree.tv",
-	def=function(x, y, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, ...)
-	standardGeneric("pstree.tv")
-)
-
-## Grouped
-setGeneric(
-      name="pstree",
-	def=function(x, group, L, nmin=1, ymin, weighted=TRUE, with.missing=FALSE, ...)
-	standardGeneric("pstree")
-)
 
 
