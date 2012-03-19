a603d3c9f414b3507fb1539e79e8f3aabdce5f67
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-19 11:26:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/pmine.R
+++ pkg/R/pmine.R
@@ -1,50 +1,47 @@
 ## Frequent patterns
 
 setMethod("pmine", signature=c(object="PSTf", data="stslist"), 
-	def=function(object, data, l, pmin, pmax, lag, state, stprob=FALSE, output="sequences") {
+	def=function(object, data, l, pmin, pmax, lag, state, average=FALSE, output="sequences") {
 	A <- alphabet(object)
 
 	prob <- predict(object, data, decomp=TRUE)
 	nbps <- rowSums(!is.na(prob))
 
-	if (!stprob) {
-		prob <- exp(rowSums(log(prob), na.rm=TRUE)/nbps)
-		
-		select.seq <- if (!missing(pmin)) { prob>=pmin } else if (!missing(pmax)) { prob < pmax }
-		res <- unique(data[select.seq,])
-
-	} else {
-
+	if (!average) { 
 		prob.check <- if (!missing(pmin)) { prob>=pmin } else if (!missing(pmax)) { prob < pmax }
-		select.seq <- vector(mode="logical", length=nrow(data))
+	} else {
+		prob.check <- log(prob)
+	}
+	
+	select.seq <- vector(mode="logical", length=nrow(data))
 
-		sl <- max(seqlength(data))
+	sl <- max(seqlength(data))
 
-		patterns.list <- NULL
+	patterns.list <- NULL
 
-		if (missing(lag)) {
-			lag <- 0
-		}
+	if (missing(lag)) { lag <- 0 }
 
-		if (missing(l)) {
-			l <- sl-lag
-		}
+	if (missing(l)) { l <- sl-lag }
 
-		for (p in (1+lag):(sl-l+1)) {
-			fp <- rowSums(prob.check[, p:(p+l-1)])==l
-			select.seq[fp] <- TRUE
-			if (output=="patterns") {
-				tmp <- seqconc(data[fp,  1:(p+l-1)])
-				patterns.list <- c(patterns.list, unique(tmp[!tmp %in% patterns.list]))
-			}
-		}
-
-		if (output=="patterns") {
-			nr <- if ("*" %in% A) { "#" } else { "*" }
-			res <- seqdef(patterns.list, alphabet=A, labels=object@labels, cpal=cpal(object), nr=nr)
+	for (p in (1+lag):(sl-l+1)) {
+		if (average) {
+			tmp <- exp(rowSums(prob.check[, p:(p+l-1)], na.rm=TRUE)/nbps)
+			fp <- if (!missing(pmin)) { tmp>=pmin } else if (!missing(pmax)) { tmp < pmax }
 		} else {
-			res <- unique(data[select.seq,])
+			fp <- rowSums(prob.check[, p:(p+l-1)])==l
 		}
+		select.seq[fp] <- TRUE
+		if (output=="patterns") {
+			tmp <- seqconc(data[fp,  1:(p+l-1)])
+			patterns.list <- c(patterns.list, unique(tmp[!tmp %in% patterns.list]))
+		}
+	}
+
+	if (output=="patterns") {
+		nr <- if ("*" %in% A) { "#" } else { "*" }
+		res <- seqdef(patterns.list, alphabet=A, labels=object@labels, cpal=cpal(object), nr=nr)
+	} else {
+		res <- unique(data[select.seq,])
 	}
 
 	return(res)
