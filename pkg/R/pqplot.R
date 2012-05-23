510316d97fb6b2b1e681c4b9885e14499b3d8007
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-23 15:38:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/pqplot.R
+++ pkg/R/pqplot.R
@@ -2,26 +2,25 @@
 ## and outcomes of the gain function 
 
 setMethod("pqplot", signature=c(object="PSTf", data="stslist"), 
-	def <- function(object, data, L, stcol=TRUE, plotseq=FALSE, ptype="barplot", cex.plot=1, space=0, grid=TRUE, seqprob=TRUE,
+	def <- function(object, data, cdata, L, stcol=TRUE, plotseq=FALSE, ptype="barplot", cex.plot=1, space=0, grid=TRUE, seqprob=TRUE,
 	measure="prob", pqmax, ...) {
-	A <- object@alphabet
-	cpal <- cpal(data)
-	nbstate <- length(A)
-	nr <- attr(data, "nr")
+
 	oolist <- list(...)
 
-	if (missing(L)) {
-		L <- length(object)-1
+	if (missing(L)) { L <- length(object)-1 }
+	
+	message(" [>] computing prob., max. length=", L)
+	
+	if (!missing(cdata)) {
+		prob <- predict(object, data=data, cdata=cdata, L=L, decomp=TRUE)
+	} else {
+		prob <- predict(object, data=data, L=L, decomp=TRUE)
 	}
-	
-	message(" [>] computing prob., max. length=", L)	
-	
-	prob <- predict(object, data=data, L=L, decomp=TRUE)
 
 	## Number of predicted symbols (used instead of sequence length)
 	sl <- rowSums(!is.na(prob))
 
-	if (measure=="log-loss") {
+	if (measure=="logloss") {
 		prob <- -log(prob, base=2)
 		if (missing(pqmax)) { pqmax <- ceiling(max(prob)) }
 		pmean <- sum(prob)/sl
@@ -36,30 +35,33 @@ setMethod("pqplot", signature=c(object="PSTf", data="stslist"),
 
 	poff <- 0
 
-	if (any(data==nr)) {
-		A <- c(A, nr)
-		cpal <- c(cpal, attr(data, "missing.color"))
-		nbstate <- nbstate+1
-	}
-
-	seqbar <- TraMineR:::seqgbar(as.matrix(data), seql=sl, statl=A)
-
 	if (plotseq) {
+		if (missing(cdata)) { cdata <- data }
+
+		c.A <- alphabet(cdata)
+		c.cpal <- cpal(cdata)
+
+		if (any(cdata==attr(cdata, "nr"))) {
+			c.A <- c(A, attr(cdata, "nr"))
+			c.cpal <- c(cpal, attr(cdata, "missing.color"))
+		}
+
+		tmp <- TraMineR:::seqgbar(as.matrix(cdata), seql=sl, statl=c.A)
+
 		## Plotting path
 		barw <- 1
 		seqscale <- 0.3
 		seqpsep <- 0.1
 		poff <- seqscale+seqpsep
 
-		seqbar <- matrix(seqbar, nrow=nbstate)
-		seqbar <- seqbar*seqscale
-		## seqbar <- cbind(seqbar, rep(0, nbstate))
+		tmp <- matrix(tmp, nrow=length(c.A))
+		tmp <- tmp*seqscale
 
 		if (!"main" %in% names(oolist)) {
 			main <- paste("States probabilities for seq")
 		}
 
-		barplot(seqbar, col=cpal, width=barw,
+		barplot(tmp, col=c.cpal, width=barw,
 			## ylab=ylab,
 			## xlim=c(0,(sl+1)),
 			ylim=c(0,(poff+1)),
@@ -73,10 +75,19 @@ setMethod("pqplot", signature=c(object="PSTf", data="stslist"),
 
 	## Plotting probability distributions
 	if (ptype=="barplot") {
-		if (stcol) {	
-			tmp <- seqbar
+		if (stcol) {
+			A <- alphabet(data)
+			cpal <- cpal(data)
+			nr <- attr(data, "nr")
+
+			if (any(data==attr(data, "nr"))) {
+				A <- c(A, nr)
+				cpal <- c(cpal, attr(data, "missing.color"))
+			}
+
+			tmp <- TraMineR:::seqgbar(as.matrix(data), seql=sl, statl=A)
 			tmp[tmp==1] <- tmp[tmp==1]*prob
-			tmp <- matrix(tmp, nrow=nbstate)
+			tmp <- matrix(tmp, nrow=length(A))
 			stcol <- cpal
 		} else {
 			tmp <- prob
