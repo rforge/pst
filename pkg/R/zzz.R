01239989e07097f4c524d4b603e9c0d7ff88bb4a
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-06 12:00:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pst@251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,7 +1,11 @@
+## welcome message
+
 .onAttach <- function(libname, pkgname){
 	suppressWarnings(descr <- utils:::packageDescription("PST"))
+
 	builtDate <- strsplit(strsplit(descr$Built, ";")[[1]][3], " ")[[1]][2]
-	packageStartupMessage("\n",descr$Package," version ", descr$Version, " (Built: ", builtDate, ")")
-	## packageStartupMessage("Website: ", descr$URL)
-	## packageStartupMessage("Please type 'citation(\"TraMineR\")' for citation information.\n")
+
+	packageStartupMessage("\n",descr$Package," ", state, " version ", descr$Version, " (Built: ", builtDate, ")")
+	packageStartupMessage("Website: ", descr$URL)
+	## packageStartupMessage("Please type 'citation(\"PST\")' for citation information.\n")
 }
